cd766017321fb6079623c37048f4b2936d633832
Nirmal-Adhikari-hub <adhikareen@gmail.com>
2025-07-16 15:37:00 +0900

    Form Local Windows

--- utils/parameters.py
+++ utils/parameters.py
@@ -126,7 +126,7 @@ def get_parser():
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # optim
     parser.add_argument(
-        '--batch-size', type=int, default=16, help='training batch size')
+        '--batch-size', type=int, default=64, help='training batch size')
     parser.add_argument(
         '--test-batch-size', type=int, default=8, help='test batch size')
 
